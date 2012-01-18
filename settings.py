aa5461eefc5cd8417338c1c0d3780ade0a2ce6a0
Kuba Janoszek <kuba@kuba.janoszek>
2012-01-18 19:46:58 +0100

    Some typos fixes.

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import os
 
 # Settings cunstomizations ##############################
-# the module can be cunstomize via DJANGO_ENV environ variableGGCC
+# the module can be cunstomize via DJANGO_ENV environ variable
 for env in ['conf.common', os.environ.get('DJANGO_ENV', 'conf.dev')]:
-    config_module = __import__('%s' % env, globals(), locals(), '%s' % env)
+    config_module = __import__(env, globals(), locals(), env)
 
     for setting in dir(config_module):
         if setting == setting.upper():
