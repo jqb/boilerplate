7aac1cfd88bcdd4602ef2cfadf615333d1a2569a
Leszek Piątek <lpiatek@gmail.com>
2012-01-23 15:34:53 +0100

    added some readability...

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,11 +4,16 @@ def projectpath(*a):
     from os.path import join, abspath
     return join('/'.join(abspath(__file__).split('/')[0:-2]), *a)
 
-# overwriting settings in order
-# settings.default, settings.dev (or settings.DJANGO_ENV), settings.local
-for s in ['default', os.environ.get('DJANGO_ENV', 'dev'), 'local']:
-    if os.path.exists('%s/%s.py' % (projectpath('settings'), s)):
-        execfile('%s/%s.py' % (projectpath('settings'), s))
+files_base_names = [
+    'default',
+    os.environ.get('DJANGO_ENV', 'dev'),
+    'local_settings'
+]
+
+for base_name in files_base_names:
+    filepath = '%s/%s.py' % (projectpath('settings'), base_name)
+    if os.path.exists(filepath):
+        execfile(filepath)
 
 # cleanup
-del os
+del base_name, files_base_names
