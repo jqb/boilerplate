a4f1060a2c90e6ada3a13b5aaef9fe0cf1150e00
Kuba Janoszek <kuba.janoszek@gmail.com>
2012-05-10 12:08:07 +0200

    setup.py url fixes

--- setup.py
+++ setup.py
@@ -11,14 +11,17 @@ def package_data(path):
     return data_files
 
 
+version = 'ver-1.0-alpha'
+
+
 setup(
     name='boilerplate',
-    version='1.0 alpha',
+    version=version,
     description='boilerplate tool for painless project layout templating',
     author='Kuba Janoszek & Leszek Piatek',
     author_email='kuba.janoszek@gmail.com, lpiatek@gmail.com',
     include_package_data=True,
-    url='https://github.com/jqb/boilerplate',
+    url='https://github.com/jqb/boilerplate/tree/%s' % version,
     packages=find_packages(),
     package_data=package_data(osjoin("boilerplate", "tmpl")),
     classifiers=[
